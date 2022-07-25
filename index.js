14f33c54d89424894e513e1932890ab7f199ea8d
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2022-07-25 13:27:03 +0600

    'practice'

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const myFun = () => {
+    if (document.getElementsByClassName('right')[0].style.display == 'block') {
+        document.getElementsByClassName('right')[0].style.display = 'none'
+    }
+    else {
+        document.getElementsByClassName('right')[0].style.display = 'block'
+    }
+
+}
